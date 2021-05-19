b39ca15dc82ed569ae90e11793028ecbb276bedf
umer baig <umerbaig1125@gmail.com>
2021-05-19 15:00:01 +0500

    day 9 complete

---
+++ lib/models/catalog.dart
@@ -0,0 +1,22 @@
+class Item {
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({this.id, this.name, this.desc, this.price, this.color, this.image});
+}
+
+final products = [
+  Item(
+    id: "Codepur001",
+    name: "iPhone 12 pro",
+    desc: "Apple iPhone 12th generation",
+    price: 999,
+    color: "#33505",
+    image:
+        "https://www.apple.com/newsroom/images/product/iphone/standard/Apple_announce-iphone12pro_10132020_big.jpg.large.jpg",
+  )
+];
