464970d482621d2a748833098267aa3f830f49f2
umer baig <umerbaig1125@gmail.com>
2021-05-21 22:40:08 +0500

    day11 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,39 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_catalog/models/catalog.dart';
 import 'package:flutter_catalog/widgets/drawer.dart';
 import 'package:flutter_catalog/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson =
+        await rootBundle.loadString("assets/files/catalog.json");
+    final decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
-    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
-
+    //for showing single item multiple time
+    //final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
+    //*************
     return Scaffold(
       appBar: AppBar(
         title: Text(
@@ -16,14 +42,23 @@ class HomePage extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(
-          itemCount: dummyList.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: dummyList[index],
-            );
-          },
-        ),
+        child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+            ? ListView.builder(
+                //itemCount: dummyList.length,
+
+                //for showing item one time
+                itemCount: CatalogModel.items.length,
+                //*********** */
+
+                itemBuilder: (context, index) => ItemWidget(
+                      //item: dummyList[index],
+                      //for showing item one time
+                      item: CatalogModel.items[index],
+                      //*********** */
+                    ))
+            : Center(
+                child: CircularProgressIndicator(),
+              ),
       ),
       drawer: MyDrawer(),
     );
